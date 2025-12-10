f7accd0691c34aab34707dde6f5c1b50cee53b06
Uchyy <uchenwaobiala@gmail.com>
2025-12-10 02:26:22 +0000

    Add smooth scrolling functionality to anchors

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // -----------------------------
 // Smooth scrolling (older browsers safe)
 // -----------------------------
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     const targetId = this.getAttribute('href').substring(1);
